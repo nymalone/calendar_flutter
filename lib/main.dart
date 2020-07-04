e65f251c4a6b265f96586bc64636777158d12190
nykolle <malone.nykolle@gmail.com>
2020-07-04 16:52:46 -0300

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'W3.Care Calendar',
+      title: 'Flutter Calendar',
       theme: ThemeData(
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity),
